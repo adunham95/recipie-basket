a399b9078abc274bd528880c1c63cd39053ae53c
Adrian <adunham95@gmail.com>
2023-05-30 11:03:49 -0400

    Added basic auth

---
+++ app/profile/page.tsx
@@ -0,0 +1,12 @@
+import { User } from '@/components/user';
+import React from 'react';
+
+const Profile = async () => {
+  return (
+    <div>
+      <User />
+    </div>
+  );
+};
+
+export default Profile;
