fece05840201adc01306f94fe7bbe635f11d0ae2
Adrian <adunham95@gmail.com>
2023-05-30 12:14:36 -0400

    Added branding

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#8aae92',
+          dark: '#6c9976',
+          light: '#a8c3ae',
+          'extra-light': '#c6d8ca',
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -15,4 +23,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
