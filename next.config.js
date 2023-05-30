4ee4f13d2af1c840fb918b1353d7da707ec48e69
Adrian <adunham95@gmail.com>
2023-05-30 15:39:39 -0400

    Clean up

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ const runtimeCaching = require('next-pwa/cache');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const withPWA = require('next-pwa')({
-  disable: !isProduction,
-  dest: 'public',
-  runtimeCaching,
-});
-
 const nextConfig = {
   pwa: {
     disable: !isProduction,
