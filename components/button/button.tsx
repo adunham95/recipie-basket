e3be923851968c0d4285569bd878e4699b5bf58b
Adrian <adunham95@gmail.com>
2023-06-01 12:22:43 -0400

    Added button component

---
+++ components/button/button.tsx
@@ -0,0 +1,90 @@
+import Link from 'next/link';
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface IButtonProps {
+  type?: 'button' | 'submit';
+  onClick?: () => void;
+  variant?: 'filled' | 'text' | 'outline';
+  className?: string;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
+  children: React.ReactNode;
+  disabled?: boolean;
+  href?: string;
+}
+
+export const Button = (props: IButtonProps) => {
+  const {
+    type = 'button',
+    onClick,
+    variant = 'filled',
+    size = 'md',
+    className = '',
+    children,
+    disabled = false,
+    href,
+  } = props;
+
+  function getVariant() {
+    switch (variant) {
+      case 'filled':
+        return 'bg-primary text-white font-semibold shadow-sm hover:bg-primary-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-dark';
+      case 'outline':
+        return 'text-primary border-2 border-primary font-semibold shadow-sm hover:text-white hover:bg-primary-dark hover:border-primary-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-dark';
+      case 'text':
+        return 'uppercase text-primary font-semibold hover:bg-primary-light hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-dark';
+      default:
+        return '';
+    }
+  }
+
+  function getSize() {
+    switch (size) {
+      case 'xs':
+        return 'rounded px-2 py-1 rounded text-xs';
+      case 'sm':
+        return 'rounded px-2 py-1 rounded text-sm';
+      case 'md':
+        return 'rounded-md px-2.5 py-1.5 text-sm';
+      case 'lg':
+        return 'rounded-md px-3 py-2 text-sm';
+      case 'xl':
+        return 'rounded-md px-3.5 py-2.5 text-sm';
+      case 'xxl':
+        return 'rounded-md px-4 py-3 text-lg';
+      default:
+        break;
+    }
+  }
+
+  if (onClick) {
+    return (
+      <button
+        onClick={onClick}
+        type={type}
+        disabled={disabled}
+        className={twMerge(getVariant(), getSize(), className)}
+      >
+        {children}
+      </button>
+    );
+  }
+
+  if (href) {
+    return (
+      <Link href={href} className={twMerge(getVariant(), getSize(), className)}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button
+      disabled={disabled}
+      type={type}
+      className={twMerge(getVariant(), getSize(), className)}
+    >
+      {children}
+    </button>
+  );
+};
--- components/profileForm.tsx
+++ components/profileForm.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import TextInput from './inputs/text-input';
 import { useSession } from 'next-auth/react';
 import { useUpdateUser } from '@/grahql-hook/mutation/updateUser';
+import { Button } from './button/button';
 
 export const ProfileForm = () => {
   const { data: session, update } = useSession();
@@ -106,47 +107,14 @@ export const ProfileForm = () => {
                 onChange={handleChange}
               />
             </div>
-
-            {/* <div className="col-span-full">
-              <label
-                htmlFor="photo"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Photo
-              </label>
-              <div className="mt-2 flex items-center gap-x-3">
-                <svg
-                  className="h-12 w-12 text-gray-300"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <button
-                  type="button"
-                  className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                >
-                  Change
-                </button>
-              </div> 
-            </div> */}
           </div>
         </div>
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button
-          disabled={isLoading}
-          type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
+        <Button disabled={isLoading} type="submit" size="lg">
           Save
-        </button>
+        </Button>
       </div>
     </form>
   );
