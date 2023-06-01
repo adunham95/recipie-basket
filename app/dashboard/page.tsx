8befc888420382929eb3c0b38ebc7f21a0c576dd
Adrian <adunham95@gmail.com>
2023-06-01 15:43:45 -0400

    Updated styles

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,7 +9,23 @@ const Dashboard = () => {
       <FilterHeader />
 
       <Container>
-        <section className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <section className="grid gap-x-6 gap-y-10 grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          <RecipeCardSmall
+            imgSrc="/images/sushi_placeholder.jpg"
+            imgAlt="sushi"
+          />
+          <RecipeCardSmall
+            imgSrc="/images/pancake_placeholder.jpg"
+            imgAlt="pancake"
+          />
+          <RecipeCardSmall
+            imgSrc="/images/seafood_placeholder.jpg"
+            imgAlt="seafood"
+          />
+          <RecipeCardSmall
+            imgSrc="/images/charcuterieboard_placeholder.jpg"
+            imgAlt="charcuterie"
+          />
           <RecipeCardSmall
             imgSrc="/images/sushi_placeholder.jpg"
             imgAlt="sushi"
